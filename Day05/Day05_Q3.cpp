ba3142e1ef055b92b8413d5c552a83204b9e9a61
ChandanRajai <140834639+ChandanRajai@users.noreply.github.com>
2023-10-01 05:37:08 +0000

    Character from ‘Z’ to ‘A’

---
+++ Day05/Day05_Q3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char n;
+
+    cout<<"Character from ‘Z’ to ‘A’"<<endl;
+
+    for(n='Z';n>='A';n--){
+        cout<<n<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
